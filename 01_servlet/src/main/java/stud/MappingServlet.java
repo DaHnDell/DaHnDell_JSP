a1567b881c96cad25bf2d6ffd2b449347832b258
DaHnDell <khanman1208@gmail.com>
2024-11-01 13:23:15 +0900

    회원가입 로그인 폼 추가

--- 01_servlet/src/main/java/stud/MappingServlet.java
+++ 01_servlet/src/main/java/stud/MappingServlet.java
@@ -1,5 +1,15 @@
 package stud;
 
-public class MappingServlet {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MappingServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("mapping");
+	}
 }
