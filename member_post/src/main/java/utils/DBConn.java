85bc65e2054b8714e76a336ef966d0fa8f473b6f
DaHnDell <khanman1208@gmail.com>
2024-11-08 14:50:10 +0900

    gitIgnore 추가

--- member_post/src/main/java/utils/DBConn.java
+++ member_post/src/main/java/utils/DBConn.java
@@ -1,7 +1,7 @@
 package utils;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -9,14 +9,25 @@ import java.util.Properties;
 
 public class DBConn {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
-		Class.forName("org.mariadb.jdbc.Driver");
-		return DriverManager.getConnection("jdbc:mariadb://54.180.153.135:3306/post", "sample", "1234");
+		Properties props = new Properties();
+		InputStream is = DBConn.class.getClassLoader().getResourceAsStream("db.properties");
+		try {
+			props.load(is);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		String driver = props.getProperty("driver");
+		String host = props.getProperty("host");
+		String username = props.getProperty("username");
+		String password = props.getProperty("password");
+		Class.forName(driver);
+		return DriverManager.getConnection(host, username, password);
 	}
 	
-	public static void main(String[] args) throws FileNotFoundException {
-		Properties props = new Properties();
-		Properties ps = System.getProperties();
-		FileReader fileReader = new FileReader("./");
-		System.out.println(fileReader);
-	}
+//	public static void main(String[] args) throws FileNotFoundException {
+//		Properties props = new Properties();
+//		Properties ps = System.getProperties();
+//		FileReader fileReader = new FileReader("./");
+//		System.out.println(fileReader);
+//	}
 }
