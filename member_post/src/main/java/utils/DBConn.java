5778b50215a047fbd1f5538c586719a7d5de5bf5
DaHnDell <khanman1208@gmail.com>
2024-11-08 14:43:14 +0900

    classpath, 경로 및 build path들 수정

--- member_post/src/main/java/utils/DBConn.java
+++ member_post/src/main/java/utils/DBConn.java
@@ -1,12 +1,22 @@
 package utils;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class DBConn {
 	public static Connection getConnection() throws ClassNotFoundException, SQLException {
 		Class.forName("org.mariadb.jdbc.Driver");
 		return DriverManager.getConnection("jdbc:mariadb://54.180.153.135:3306/post", "sample", "1234");
+	}
+	
+	public static void main(String[] args) throws FileNotFoundException {
+		Properties props = new Properties();
+		Properties ps = System.getProperties();
+		FileReader fileReader = new FileReader("./");
+		System.out.println(fileReader);
 	}
 }
