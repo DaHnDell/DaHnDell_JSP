44dc1196fd912458379d2024254da089e2de32f7
DaHnDell <khanman1208@gmail.com>
2024-11-11 18:20:29 +0900

    parentPath 경로 추가 + mkdirs로 날짜별 구분

--- member_post/src/main/java/servlet/common/Upload.java
+++ member_post/src/main/java/servlet/common/Upload.java
@@ -2,7 +2,10 @@ package servlet.common;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -11,7 +14,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.fileupload.FileItem;
-import org.apache.commons.fileupload.FileUploadException;
 import org.apache.commons.fileupload.disk.DiskFileItemFactory;
 import org.apache.commons.fileupload.servlet.ServletFileUpload;
 
@@ -22,24 +24,41 @@ public class Upload extends HttpServlet{
 		DiskFileItemFactory factory = new DiskFileItemFactory();
 		factory.setSizeThreshold(1024*1024);
 		factory.setRepository(new File("c:/upload/tmp"));
-		
+		// 파일 이름 생성 규칙을 알아야 함.
 		ServletFileUpload upload = new ServletFileUpload(factory);
 		List<FileItem> items;
 		try {
 			items = upload.parseRequest(req);
-			items.forEach(item->{
+			for(FileItem item : items) {
+//			items.forEach(item->{
+//				item.getFieldName() files
 				System.out.println(item.getName());
 				System.out.println(item.getSize());
-			});
-		} catch (FileUploadException e) {
-			// TODO Auto-generated catch block
+				
+				String origin = item.getName();
+				int dotIdx = origin.lastIndexOf(".");
+				String ext = "";
+				if(dotIdx != -1) {
+					origin.substring(dotIdx);
+				}
+				String realName = UUID.randomUUID() + ext;
+				File parentPath = new File("c:/upload", getTodayStr());
+				if(!parentPath.exists()) {
+					parentPath.mkdirs();
+					// s 안붙은 애들은 마지막 하나만 만들어주는 메서드라서 무조건 s 붙은걸로 써야 함.
+				}
+				item.write(new File(parentPath, realName));
+				
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-				
-
 	}
 	
+	public String getTodayStr() {
+		return new SimpleDateFormat("yyyy/MM/dd").format(System.currentTimeMillis());
+	}
 	
 
 }
